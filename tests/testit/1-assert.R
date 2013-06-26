f7a419863837c3c706bc2fbc9c941b798ae9a704
Yihui Xie <xie@yihui.name>
2013-06-26 16:12:13 -0700

    state facts in assert() instead of questions

--- tests/testit/1-assert.R
+++ tests/testit/1-assert.R
@@ -2,13 +2,14 @@ library(testit)
 
 assert('assert works', 1==1)
 
+# Okay, that is kind of cheating
 assert(
-  'Okay, that is kind of cheating; test assert() itself now',
+  'assert() should signal an error if a condition does not hold',
   has_error(assert('this should produce an error', 1==2))
 )
 
 # a meaningless test in terms of R (failure is irrelevant to Frequentist or Bayesian)
 try(assert(
-  'Are Frequentists correct? (http://xkcd.com/1132/)',
+  'Frequentists must be correct (http://xkcd.com/1132/)',
   'The sun has exploded!', sample(6, 2) == c(6, 6)
 ), silent = !interactive())
