99c72108ca4d90be7afa84389a68968dc80c2ddc
Yihui Xie <xie@yihui.name>
2016-02-16 17:17:03 -0600

    add the utils qualifier

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,9 @@
 # find an available dir
 available_dir = function(dirs) {
   for (i in dirs) {
-    if (file_test('-d', i)) return(i)
+    if (utils::file_test('-d', i)) return(i)
   }
-  stop('none of the directories exists:\n', paste(formatUL(dirs), collapse = '\n'))
+  stop('none of the directories exists:\n', paste(utils::formatUL(dirs), collapse = '\n'))
 }
 
 # tailored for assert(): extract the expression that is likely to be useful for
