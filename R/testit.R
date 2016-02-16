3aaeb051f3c120d73c9430e203b80eb280ce5732
Yihui Xie <xie@yihui.name>
2016-02-16 16:39:39 -0600

    add an infix operator %==%

--- R/testit.R
+++ R/testit.R
@@ -1,9 +1,9 @@
 #' Assertions with a message
 #'
-#' This function was built from \code{\link{stopifnot}()}. It emits a message in
-#' case of errors, which can be a helpful hint for diagnosing the errors
-#' (\code{stopifnot()} only prints the possibly truncated source code of the
-#' expressions).
+#' The function \code{assert()} was built from \code{\link{stopifnot}()}. It
+#' emits a message in case of errors, which can be a helpful hint for diagnosing
+#' the errors (\code{stopifnot()} only prints the possibly truncated source code
+#' of the expressions).
 #' @param fact a message for the assertions when any of them fails; treated the
 #'   same way as expressions in \code{...} if it is not a character string,
 #'   which means you do not have to provide a message to this function
@@ -51,6 +51,14 @@ assert = function(fact, ...) {
   }
 }
 
+#' @description The infix operator \code{\%==\%} is simply an alias of the
+#'   \code{\link{identical}()} function to make it slightly easier and intuitive
+#'   to write test conditions. \code{x \%==\% y} is the same as
+#'   \code{identical(x, y)}.
+#' @param x,y two R objects to be compared
+#' @rdname assert
+#' @export
+`%==%` = function(x, y) identical(x, y)
 
 #' Run the tests of a package in its namespace
 #'
