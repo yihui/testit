119b179b68ca85e9cdfe1e10d2f140fbb1c17e8c
Yihui Xie <xie@yihui.name>
2016-11-08 15:57:21 -0600

    cosmetic

--- tests/testit/test-assert.R
+++ tests/testit/test-assert.R
@@ -1,11 +1,11 @@
 library(testit)
 
-assert('assert works', 1==1)
+assert('assert works', 1 == 1)
 
 # Okay, that is kind of cheating
 assert(
   'assert() should signal an error if a condition does not hold',
-  has_error(assert('this should produce an error', 1==2))
+  has_error(assert('this should produce an error', 1 == 2))
 )
 
 # a meaningless test in terms of R (failure is irrelevant to Frequentist or Bayesian)
